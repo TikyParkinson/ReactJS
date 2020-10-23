b40be99cc3a332bfffb58b87517c5909c48a995a
Antonio <antonio507murcia@gmail.com>
2020-10-23 23:19:40 +0200

    PropTypes

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,14 +1,20 @@
 //import React, { Fragment } from 'react';
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // FC
-const PrimeraApp = ({ saludo = 'Hola Mundo' }) => {
+const PrimeraApp = ({ saludo, edad }) => {
   return (
     <>
       <h1>{saludo}</h1>
-      <p>Mi Primera Aplicacion</p>
+      <p>Mi Primera Aplicacion {edad}</p>
     </>
   );
+};
+
+PrimeraApp.propTypes = {
+  saludo: PropTypes.string.isRequired,
+  edad: PropTypes.number.isRequired,
 };
 
 export default PrimeraApp;
