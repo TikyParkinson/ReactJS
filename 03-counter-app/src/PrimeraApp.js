35ac7986ac5eaa9e2be12e2ae8e8cbb140866c96
Antonio <antonio507murcia@gmail.com>
2020-10-23 22:53:56 +0200

    Retornar elementos con y sin fragment

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,8 +1,14 @@
+//import React, { Fragment } from 'react';
 import React from 'react';
 
 // FC
 const PrimeraApp = () => {
-  return <h1>Hola mundo</h1>;
+  return (
+    <>
+      <h1>Hola mundo</h1>
+      <p>Mi Primera Aplicacion</p>
+    </>
+  );
 };
 
 export default PrimeraApp;
