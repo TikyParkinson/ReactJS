d263e717c713e8b266c37b2e249b38abecf18af2
Antonio <antonio507murcia@gmail.com>
2020-10-25 12:02:12 +0100

    Use states Hook

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,17 +1,19 @@
-//import React, { Fragment } from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 // FC
 const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(0);
+
   const handleAdd = (e) => {
-    console.log(e);
+    // setCounter(counter + 1);
+    setCounter((c) => c + 1);
   };
 
   return (
     <>
       <h1>CounterApp</h1>
-      <p>{value}</p>
+      <p>{counter}</p>
 
       <button onClick={handleAdd}>+1</button>
     </>
