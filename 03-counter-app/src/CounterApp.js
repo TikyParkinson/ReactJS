620728d02ae8d9c5c1e1a8079e1f0c5e3432c322
Antonio <antonio507murcia@gmail.com>
2020-10-25 11:49:49 +0100

    Eventos de Click

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -4,10 +4,16 @@ import PropTypes from 'prop-types';
 
 // FC
 const CounterApp = ({ value }) => {
+  const handleAdd = (e) => {
+    console.log(e);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
       <p>{value}</p>
+
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 };
